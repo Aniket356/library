a2e6580eda5a773f856d584227b6d65d4dc57954
Aniket <t.aniket561@gmail.com>
2023-07-25 22:20:08 +0530

    add functionality to add books through form

--- script.js
+++ script.js
@@ -1,45 +1,60 @@
 let myLibrary = []
 const booksContainer = document.querySelector('.books');
 
-function Book(name, author, isRead){
-    this.name = name;
+function Book(title, author, isRead){
+    this.title = title;
     this.author = author;
     this.isRead = isRead;
 }
 
-function addBookToLibrary(name, author, isRead){
-    const book = new Book(name, author, isRead);
-    myLibrary.push(book);
+function addBookToLibrary(title, author, isRead){
+    const newBook = new Book(title, author, isRead);
+    myLibrary.push(newBook);
     showBooks();
 }
 
-function removeBook(index){
-    myLibrary.splice(index, 1);
-}
-
 function showBooks() {
-    booksContainer.innerHTML = "";
+
+    while(booksContainer.hasChildNodes()){
+        booksContainer.removeChild(booksContainer.firstChild);
+    }
+
     myLibrary.forEach(book => {
-
         const newBook = document.createElement('div');
-        newBook.classList.add('book')
+        newBook.classList.add('book');
 
-        const bookName = document.createElement('p');
-        bookName.innerText = book.name;
-        bookName.classList.add('book-name');
+        const bookTitle = document.createElement('p');
+        bookTitle.classList.add('title');
+        bookTitle.textContent = book.title;
+        newBook.appendChild(bookTitle);
 
         const bookAuthor = document.createElement('p');
-        bookAuthor.innerText = book.author;
-        bookAuthor.classList.add('book-author');
-
-        const isReadBook = document.createElement('button');
-        isReadBook.innerText = `${book.isRead ? 'Read' : 'Not read'}`
-        isReadBook.classList.add('book-is-read');
-
-        newBook.appendChild(bookName);
+        bookAuthor.classList.add('author');
+        bookAuthor.textContent = book.author;
         newBook.appendChild(bookAuthor);
-        newBook.appendChild(isReadBook);
 
-        booksContainer.appendChild(newBook)
+        const bookReadBtn = document.createElement('button');
+        bookReadBtn.classList.add(`${book.isRead ? 'read' : 'not-read'}`);
+        bookReadBtn.textContent = `${book.isRead ? 'Already Read' : 'Not Read Yet'}`
+        newBook.appendChild(bookReadBtn);
+
+        const removeBookBtn = document.createElement('button');
+        removeBookBtn.textContent = 'Delete Book';
+        newBook.appendChild(removeBookBtn);
+
+        booksContainer.appendChild(newBook);
     })
 }
+
+const formSubmitBtn = document.querySelector('.submit-btn > button');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const bookIsReadInput = document.querySelector('#have-read');
+formSubmitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    // console.log(`Title: ${titleInput.value}`);
+    // console.log(`Author: ${authorInput.value}`);
+    // console.log(`Is Read: ${bookIsReadInput.value}`);
+
+    addBookToLibrary(titleInput.value, authorInput.value, bookIsReadInput.checked);
+})
